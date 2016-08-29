2815e85992dcb4a20d15ec971b3bcf788d85df3e
Raman Pandey <raman.pndy@gmail.com>
2016-08-29 16:09:47 +0530

    0/1 Knapsack Problem Solved Using Recursion

---
+++ 1 Knapsack.py
@@ -0,0 +1,24 @@
+# recursive implementation of 0/1 knapsack problem
+def knapsack(W,wt,val,n):
+    # return the maximum value that can be put in a knapsack of weight W.
+    # Base Case
+    if n==0 or W==0:
+        return 0
+
+    # if weight of the nth item is greater than W then it would not be included in the optimal solution
+    if (wt[n-1] > W):
+        return knapsack(W,wt,val,n-1)
+
+    # return the maximum of two cases
+    # 1.nth item included
+    # 2.nth item not included
+
+    else:
+        return max(val[n-1] + knapsack(W-wt[n-1],wt,val,n-1),knapsack(W,wt,val,n-1))
+
+val = [60,100,120]
+wt = [10,20,30]
+W =50
+n = len(val)
+
+print knapsack(W,wt,val,n)
