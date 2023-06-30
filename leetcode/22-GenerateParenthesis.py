c20aa78b8c8a597201fe0ac246a23ad729b0f959
RamanPndy <raman.pandey@bookmyshow.com>
2023-06-30 11:52:20 +0530

    added problems approach

--- leetcode/22-GenerateParenthesis.py
+++ leetcode/22-GenerateParenthesis.py
@@ -3,6 +3,12 @@ class Solution(object):
         """
         :type n: int
         :rtype: List[str]
+        Input: n = 3
+        Output: ["((()))","(()())","(())()","()(())","()()()"]
+        1. The idea is to add ')' only after valid '('
+        2. We use two integer variables left & right to see how many '(' & ')' are in the current string
+        3. If left < n then we can add '(' to the current string
+        4. If right < left then we can add ')' to the current string
         """
         l, r = 0, 0
         q = [(l, r, '')]
--- leetcode/76-MinimumWindowSubstring.py
+++ leetcode/76-MinimumWindowSubstring.py
@@ -9,6 +9,17 @@ class Solution(object):
         Input: s = "ADOBECODEBANC", t = "ABC"
         Output: "BANC"
         Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+        1. Check if s is shorter than t. If it is, there is no possible solution, and we return an empty string.
+        2. Create a frequency map of characters in t.
+        3. Initialize have, left, min_length, and l to 0.
+        4. Traverse the string s using the end pointer.
+        5. If the current character in s is present in the frequency map, increment the have.
+        6. Decrement the frequency of the current character in the frequency map.
+        7. If the have equals the need, it means we have found a window that contains all characters of t. 
+        Now we try to shrink the window by moving the left pointer forward until the window still contains all the characters of t.
+        8. If the length of the current window is smaller than the minimum length so far, update the minimum length and the minimum start.
+        9. Increment the frequency of the character at the left pointer and decrement the count.
+        10.Return the minimum window or an empty string if no window exists.
         """
         if not t: return ""
 
