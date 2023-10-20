78ddea558501bf33ed8b175d6d8ab4a06d0fe302
RamanPndy <raman.pandey@bookmyshow.com>
2023-10-20 15:36:38 +0530

    added kubernetes concepts

---
+++ concepts/golang/oops.go
@@ -0,0 +1,98 @@
+// Polymorphism
+type Instrument interface {
+	Play()
+}
+
+func PlayInstrument(i Instrument) {
+	i.Play()
+}
+
+type Guitar struct {
+	Type string
+}
+
+func (g Guitar) Play() {
+	fmt.Println("Guitar Sounds")
+}
+
+func main() {
+	g := Guitar{"Classical"}
+	PlayInstrument(g)
+}
+
+// Constructors
+type Customer struct {
+	Name string
+	Age  int
+}
+
+func (c *Customer) NewCustomer(name string, age int) {
+	c.Name = name
+	c.Age = age
+}
+
+func main() {
+	customer := new(Customer)
+	customer.NewCustomer("John", 30)
+	fmt.Printf("%s: %d\n", customer.Name, customer.Age)
+}
+
+// Inheritance
+type Vehicle struct {
+	Seats int
+	Color string
+}
+
+type Car struct {
+	Vehicle
+}
+
+type MotorCycle struct {
+	Base Vehicle
+}
+
+func main() {
+	car := &Car{
+		Vehicle{
+			Seats: 4,
+			Color: "blue",
+		},
+	}
+
+	fmt.Println(car.Seats)
+	fmt.Println(car.Color)
+}
+
+func main() {
+	motorCycle := &MotorCycle{
+		Vehicle{
+			Seats: 2,
+			Color: "red",
+		},
+	}
+
+	fmt.Println(motorCycle.Base.Seats)
+	fmt.Println(motorCycle.Base.Color)
+}
+
+// Encapsulation
+type Customer struct {
+	id   int
+	name string
+}
+
+func (c *Customer) GetID() int {
+	return c.id
+}
+
+func (c *Customer) GetName() string {
+	return c.name
+}
+
+// Enums
+const (
+	Guitar = iota // 0
+	Violin        // 1
+	Piano         // 2
+	Drums         // 3
+)
--- systemdesign/AppointmentBookinSystem.py
+++ systemdesign/AppointmentBookinSystem.py
@@ -1,6 +1,9 @@
+from enum import Enum
+class Status(Enum):
+    BOOKED, CONFIRMED, CANCELLED = 1,2,3
 class User:
     def __init__(self, user_id, username, email, password, role):
-        self.user_id = user_id
+        self.id = user_id
         self.username = username
         self.email = email
         self.password = password
@@ -8,7 +11,7 @@ class User:
 
 class Service:
     def __init__(self, service_id, name, description, duration, provider_id, availability):
-        self.service_id = service_id
+        self.id = service_id
         self.name = name
         self.description = description
         self.duration = duration
@@ -16,11 +19,10 @@ class Service:
         self.availability = availability
 
 class Appointment:
-    def __init__(self, appointment_id, customer_id, service_id, provider_id, date_time, status):
-        self.appointment_id = appointment_id
+    def __init__(self, appointment_id, customer_id, service_id, date_time, status):
+        self.id = appointment_id
         self.customer_id = customer_id
         self.service_id = service_id
-        self.provider_id = provider_id
         self.date_time = date_time
         self.status = status
 
@@ -40,16 +42,23 @@ class AppointmentBookingSystem:
         self.services[service_id] = service
         return service
 
-    def book_appointment(self, customer_id, service_id, provider_id, date_time):
+    def book_appointment(self, customer_id, service_id, date_time):
         appointment_id = len(self.appointments) + 1
-        appointment = Appointment(appointment_id, customer_id, service_id, provider_id, date_time, "Booked")
+        appointment = Appointment(appointment_id, customer_id, service_id, date_time, Status.BOOKED)
         self.appointments[appointment_id] = appointment
         return appointment
 
     def confirm_appointment(self, appointment_id):
         appointment = self.appointments.get(appointment_id)
         if appointment:
-            appointment.status = "Confirmed"
+            appointment.status = Status.CONFIRMED
+            return True
+        return False
+
+    def cancel_appointment(self, appointment_id):
+        appointment = self.appointments.get(appointment_id)
+        if appointment:
+            appointment.status = Status.CANCELLED
             return True
         return False
 
@@ -61,5 +70,6 @@ provider = booking_system.create_user(2, "JaneSmith", "jane@example.com", "passw
 
 service = booking_system.create_service(1, "Haircut", "Basic haircut", 60, provider.user_id, "Mon-Fri, 9am-5pm")
 
-appointment = booking_system.book_appointment(customer.user_id, service.service_id, provider.user_id, "2023-08-20 10:00")
+appointment = booking_system.book_appointment(customer.user_id, service.service_id, "2023-08-20 10:00")
 booking_system.confirm_appointment(appointment.appointment_id)
+booking_system.cancel_appointment(appointment.appointment_id)
